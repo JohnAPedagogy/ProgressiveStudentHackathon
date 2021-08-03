71a7e09aa868fed077a8e11069c4f20c1ddafba6
John Alamina <john.alamina@gmail.com>
2021-08-03 20:12:31 +0100

    mongo-needs-fixing

---
+++ 2021/mongodb/Description.md
@@ -0,0 +1,16 @@
+
+
+# MongoDB Database
+
+
+```
+  {
+    "GroupID": 1,
+    "ID": 1,
+    "LastName":"Carson",
+    "OtherNames":"Alexander",
+    "SGCode" : "",
+    "UniCode":"u1273400",
+    "StartDate":"2021-01-01"
+  }
+```
---
+++ 2021/mongodb/index.js
@@ -0,0 +1,43 @@
+const client = stitch.Stitch.initializeDefaultAppClient('sdb-0-fvshj');
+const db = client.getServiceClient(stitch.RemoteMongoClient.factory,'mongodb-atlas').db('progressive');
+
+function run_query(q) {
+  return client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => {
+    // document.getElementById('auth-status').innerHTML = 
+    //   `Logged in ${user.id}`;
+      console.dir(user);
+			return db.collection('Songs').aggregate(q).asArray()
+			 .then(docs => {
+			 	console.dir(docs);
+			 	 const html = docs.map(c => "<div>" + c.Title + "</div>").join("");
+			 	 document.getElementById("test").innerHTML = html;
+			 }).catch(err => {
+			 	console.error(err);
+			 })
+	});
+}
+// const app = new Realm.App({ id: "<Your App ID>" });
+// window.onload = initializeAndLogin;
+
+function samplequery() {
+	const q = [{$sort:{order_date:-1}},{$skip:hchcs.pageSkip*pageSize},{$limit:pageSize}];
+		run_query(q1).then(docs=>{ 
+					console.log("found "+docs.length+" student!");
+					hchcs.pageCount=Math.floor(docs.length/pageSize);//+1;
+		}).catch(err=> {
+				//dbg("Ooops! Something went wrong!", response.errors[0].message);
+				console.error(err);
+		});
+}
+/*
+ * 
+ * 
+GroupID:1
+ID:2,
+LastName:"Betty"
+OtherNames:"Boop"
+SGCode:""
+UniCode:"u0001111"
+StartDate:"2021-01-01"
+ * 
+ * /
