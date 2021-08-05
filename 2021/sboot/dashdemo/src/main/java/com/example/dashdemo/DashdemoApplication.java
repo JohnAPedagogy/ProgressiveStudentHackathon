dbd182a106cd5bf957e9f56536b1078aafe4f546
John Alamina <john.alamina@hud.ac.uk>
2021-08-05 10:09:03 +0100

    sb00

---
+++ 2021/sboot/dashdemo/src/main/java/com/example/dashdemo/DashdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dashdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashdemoApplication.class, args);
+	}
+
+}
---
+++ 2021/sboot/dashdemo/src/test/java/com/example/dashdemo/DashdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
