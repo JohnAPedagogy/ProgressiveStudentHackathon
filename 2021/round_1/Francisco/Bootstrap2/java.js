0e355b4c544a54ed9a11e0d9beeddb55f55c1483
John Alamina <john.alamina@gmail.com>
2021-08-16 22:40:26 +0100

    attendance md

--- 2021/round_1/Francisco/Bootstrap2/java.js
+++ 2021/round_1/Francisco/Bootstrap2/java.js
@@ -37,7 +37,7 @@ function insert_data(table,id,data){
 
 function delete_data(table,id){
     //db.posts.remove({ title: 'Post Four' })
-    db.collection(table).remove({ID:id})
+    db.collection(table).deleteOne({ID:id})
         .then(()=>{
             samplequery(table);
     }).catch(err => {
@@ -88,12 +88,3 @@ delete_data('ModuleTask', 1);
 
 
 
-
-mapboxgl.accessToken =
-'pk.eyJ1IjoiYnRyYXZlcnN5IiwiYSI6ImNrbmh0dXF1NzBtbnMyb3MzcTBpaG10eXcifQ.h5ZyYCglnMdOLAGGiL1Auw'
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [-71.060982, 42.35725],
-zoom: 18,
-})
