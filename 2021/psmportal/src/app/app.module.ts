f1c31547f672d05b5c7283434753f02db64d80fa
Francisco Hernandez <u1975966@unimail.hud.ac.uk>
2021-08-19 23:39:48 +0100

    Angular Header created

--- 2021/psmportal/src/app/app.module.ts
+++ 2021/psmportal/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from 'src/uic/layout/header/header.component';
+import { SidebarComponent } from 'src/uic/layout/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
