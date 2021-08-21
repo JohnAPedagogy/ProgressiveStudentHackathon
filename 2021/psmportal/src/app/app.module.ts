bc26dc7eaad73a63b1ce80a370a3fdcd8ff349f6
Francisco Hernandez <u1975966@unimail.hud.ac.uk>
2021-08-21 21:45:03 +0100

    Side bar improved

--- 2021/psmportal/src/app/app.module.ts
+++ 2021/psmportal/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './../uic/template/home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,8 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatExpansionModule} from '@angular/material/expansion';
 import { FormsModule } from '@angular/forms'; 
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -19,15 +22,19 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatListModule,
     NgbModule,
     BrowserAnimationsModule,
     FormsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
     MatIconModule,
     MatSidenavModule
   ],
