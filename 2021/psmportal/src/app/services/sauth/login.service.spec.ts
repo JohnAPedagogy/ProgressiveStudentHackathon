a23823100261cbbd05fddfde0b19d91b93548893
John Alamina <john.alamina@gmail.com>
2021-09-03 10:58:12 +0100

    ses00

--- 2021/psmportal/src/app/services/sauth/login.service.spec.ts
+++ 2021/psmportal/src/app/services/sauth/login.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SessionService } from './session.service';
+import { LoginService } from './login.service';
 
-describe('SessionService', () => {
-  let service: SessionService;
+describe('LoginService', () => {
+  let service: LoginService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SessionService);
+    service = TestBed.inject(LoginService);
   });
 
   it('should be created', () => {
---
+++ 2021/psmportal/src/app/services/sauth/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+}
+
+export class Session{
+  constructor(){
+    
+  }
+}
+export class SessionStatus{
+  constructor(){
+    
+  }
+}
